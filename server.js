eb46e0491be80e8449de6fabbef818290559bf89
Paul David Lampe <pdlampe@gmail.com>
2019-08-30 12:06:11 -0500

    Updated server file and new folders

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, './app/public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: " application/vnd.api+json" }));
+
 
 require("./app/routing/api-routes.js")(app);
 require("./app/routing/html-routes.js")(app);
