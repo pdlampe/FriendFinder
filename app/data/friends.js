0651cb21c466c19be4663fb2b94e8e038d19065d
Paul David Lampe <pdlampe@gmail.com>
2019-09-03 22:30:34 -0500

    Created friends JS file with first user information

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+        name: 'Paul David Lampe',
+        profilePic: 'https://avatars1.githubusercontent.com/u/49738794?s=460&v=4',
+        scores: [
+            "3",
+            "4",
+            "5",
+            "2",
+            "4",
+            "3",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    }
+]
+
+module.exports = friendList;
--- app/public/survey.js
+++ app/public/survey.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    $("#submitBtn").on("click", function () {
-        //form validation
+    $("#submitHere").on("click", function () {
+
         function validateForm() {
             var isValid = true;
             $('.validate').each(function () {
@@ -15,51 +15,51 @@ $(document).ready(function () {
             });
             return isValid;
         }
-        //if everything is filled
+
         if (validateForm() == true) {
-            //creates a new friend from the values submitted
+
             var newFriend = {
                 name: $('#name').val().trim(),
                 profilePic: $('#photo').val().trim(),
                 scores: [
-                    $('#q1').val(),
-                    $('#q2').val(),
-                    $('#q3').val(),
-                    $('#q4').val(),
-                    $('#q5').val(),
-                    $('#q6').val(),
-                    $('#q7').val(),
-                    $('#q8').val(),
-                    $('#q9').val(),
-                    $('#q10').val(),
+                    $('#question11').val(),
+                    $('#question12').val(),
+                    $('#question13').val(),
+                    $('#question14').val(),
+                    $('#question15').val(),
+                    $('#question16').val(),
+                    $('#question17').val(),
+                    $('#question18').val(),
+                    $('#question19').val(),
+                    $('#question110').val(),
                 ]
             };
-            //Grabs current URL of website
+
             var currentURL = window.location.origin;
-            //AJAX posts the data to friends API.
+
             $.post(currentURL + "/api/friends", newFriend, function (data) {
-                //Grab the result from the AJAX post so that the best match's name and photo are displayed.
+
                 $("#matchName").text(data.name);
                 $("#matchPic").attr("src", data.profilePic);
             });
-            // Show the modal with the best match
+
             $('.modal').modal();
-            //clear form after submission
+
             $('#name').val("");
             $('#photo').val("");
-            $('#q1').val("");
-            $('#q2').val("");
-            $('#q3').val("");
-            $('#q4').val("");
-            $('#q5').val("");
-            $('#q6').val("");
-            $('#q7').val("");
-            $('#q8').val("");
-            $('#q9').val("");
-            $('#q10').val("");
+            $('#question1').val("");
+            $('#question2').val("");
+            $('#question3').val("");
+            $('#question4').val("");
+            $('#question5').val("");
+            $('#question6').val("");
+            $('#question7').val("");
+            $('#question8').val("");
+            $('#question9').val("");
+            $('#question10').val("");
         } else {
-            alert("Please fill out ALL fields before submitting survey!")
+            alert("Please fill out ALL fields before submitting survey.")
         }
-        // return false;
+
     });
 });
