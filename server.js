acfa5dc46c23e713542ac7a70c0c5aad02e230f5
Paul David Lampe <pdlampe@gmail.com>
2019-09-08 15:27:57 -0500

    Updates to files to create functional program

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, './app/public')));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
 
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+    console.log("Application listening on PORT: " + PORT);
 });
