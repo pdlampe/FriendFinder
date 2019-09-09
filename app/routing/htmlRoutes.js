ee9342463b5c57cace52bfcba20d7593cfb46cae
Paul David Lampe <pdlampe@gmail.com>
2019-09-09 17:56:16 -0500

    Updated HTML routes for survey and home pages

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,6 +7,6 @@ module.exports = function (app) {
     });
 
     app.use(function (req, res) {
-        res.sendFile(path.join(__dirname + '/../public/home.html'));
+        res.sendFile(path.join(__dirname + '../public/home.html'));
     });
 };
